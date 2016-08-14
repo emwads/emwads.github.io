49c04595bdcede2559a5e945d96493bb126a1880
emwads <iamwatiel@gmail.com>
2016-08-13 18:20:52 -0700

    adjusted css styling

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,7 +3,6 @@
 const navBar = $(".top-nav");
 
 $(window).scroll(function() {
-  console.log($(this).scrollTop());
   if( $(this).scrollTop() > $('header').height() ) {
     navBar.addClass('nav-stuck');
   } else {
